1d2f1ab6897f51f3ce5dcdce941f801ac6efd6c2
Hu Min <minispring1013@gmail.com>
2017-06-26 22:56:46 +0800

    Init

---
+++ app/models/TempNode.java
@@ -0,0 +1,29 @@
+package models;
+
+/**
+ * Created by humin on 02/06/2017.
+ */
+public class TempNode implements Comparable<TempNode> {
+
+    public String name;
+
+    public String shortname;
+
+    public String displayname;
+
+    public String category;
+
+    public Boolean draggable=true;
+
+    public Integer x;
+
+    public Integer y;
+
+    @Override
+    public int compareTo(TempNode o) {
+        if(this.name != o.name){
+            return 1;
+        }
+        return 0;
+    }
+}
